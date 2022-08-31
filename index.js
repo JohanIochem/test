73d12770c8ca3b967e10cc56dd3f5d5d21d4dc53
JohanIochem <112541477+JohanIochem@users.noreply.github.com>
2022-08-31 11:51:11 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-function bienvenido()
-{
-    let div_a_écrire = document.getElementById('bienvenue');
-    div_a_écrire.innerHTML = "Bonjour et bienvenue,";
+// function bienvenido()
+// {
+//     let div_a_écrire = document.getElementById('bienvenue');
+//     div_a_écrire.innerHTML = "Bonjour et bienvenue,";
 
-    let numberOfSeasons = 6;
-    document.getElementById('saisons').innerHTML= numberOfSeasons;
+//     let numberOfSeasons = 6;
+//     document.getElementById('saisons').innerHTML= numberOfSeasons;
 
-    let numberOfEpisodes=12;
-    document.getElementById('episodes').innerHTML= numberOfEpisodes;
-}
+//     let numberOfEpisodes=12;
+//     document.getElementById('episodes').innerHTML= numberOfEpisodes;
+// }
+
+let episodeTime = 45;
+let commercialTime = 5;
+
+let totalepisodeandcommercial = (episodeTime + commercialTime) * numberOfSeasons * numberOfEpisodes;
+
+let numberOfSeasons = 6;
+        document.getElementById('episodeTime').innerHTML= episodeTime;
+
+        let numberOfEpisodes=12;
+        document.getElementById('commercialTime').innerHTML= commercialTime;
